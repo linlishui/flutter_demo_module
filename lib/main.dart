d17a6a50e5fb4332c5724f69d57abebae98eb848
linlishui <lishui.lin@qq.com>
2023-07-25 20:26:08 +0800

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -2,41 +2,5 @@ import 'package:app_demo/main.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(const FlutterProxyApp());
-}
-
-class FlutterProxyApp extends StatelessWidget {
-  const FlutterProxyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final rootWidget = MaterialApp(
-        title: 'AppDemoModule',
-        onGenerateRoute: (settings) {
-          return MaterialPageRoute(builder: (context) {
-            switch (settings.name) {
-              case "/":
-                return const PlaceholderPage();
-              case "/home":
-                return const FlutterDemoApp();
-              default:
-                return const Scaffold(
-                  body: Center(
-                    child: Text("页面不存在"),
-                  ),
-                );
-            }
-          });
-        });
-    return rootWidget;
-  }
-}
-
-class PlaceholderPage extends StatelessWidget {
-  const PlaceholderPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(color: Colors.white);
-  }
+  runApp(const FlutterDemoApp());
 }
